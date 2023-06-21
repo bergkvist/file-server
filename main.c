f851e3b81ba948d7690a7fe67808faeb17119ab0
Tobias Bergkvist <tobias@bergkv.ist>
2023-06-21 03:09:17 +0200

    Switch from file pointers to only use read/write on the socket file descriptor directly. Fix is_directory retaining state due to uninitialized stat struct.

--- main.c
+++ main.c
@@ -11,16 +11,17 @@
 
 #define assert_ok(e) ({int x = (e); if (x < 0) { printf("%s:%d: ", __FILE__, __LINE__); fflush(stdout); perror(#e); abort(); } x;})
 #define DEFAULT_HOST "127.0.0.1"
-#define DEFAULT_PORT 5000
+#define DEFAULT_PORT 1233
 #define FILE_BUFFER_SIZE 4096
+#define RECV_BUFFER_SIZE 4096
 
 
 int create_ipv4_socket_and_listen(char *host, uint16_t port);
 int parse_http_status_line(char *status_line, char **method, char **path, char **version);
 size_t read_filesize(FILE *file);
 int is_directory(const char *path);
-int render_directory_as_html(FILE *out, DIR *d, const char *path);
-
+int render_directory_as_html(char **html, size_t *html_length, DIR *d, const char *path);
+void log_http_request_response(char *method, char *path, char *response_status);
 
 int main(int argc, char **argv) {
     char *host = DEFAULT_HOST;
@@ -33,18 +34,19 @@ int main(int argc, char **argv) {
 
     while (1) {
         int connection = accept(sock, NULL, NULL);
-        char *http_status = NULL;
-        size_t http_status_length = 0;
-        FILE *connection_handle = fdopen(connection, "r+");
-        getline(&http_status, &http_status_length, connection_handle);
-        printf("http_status = %s", http_status);
-        char *response_status;
+        char http_status[RECV_BUFFER_SIZE];
+        read(connection, http_status, RECV_BUFFER_SIZE);
+
+        char *response = NULL;
+        size_t response_length = 0;
+        FILE *response_stream = open_memstream(&response, &response_length);
 
         char *http_method, *http_path;
         if (parse_http_status_line(http_status, &http_method, &http_path, NULL) < 0) {
-            response_status = "400 BAD REQUEST";
-            fprintf(connection_handle, "HTTP/1.1 %s\r\nContent-Length: 16\r\n\r\n400 Bad Request\n", response_status);
-            fflush(connection_handle);
+            log_http_request_response(http_method, http_path, "400 BAD REQUEST");
+            fprintf(response_stream, "HTTP/1.1 400 BAD REQUEST\r\nContent-Length: 16\r\n\r\n400 Bad Request\n");
+            fflush(response_stream);
+            write(connection, response, response_length);
             goto cleanup;
         }
 
@@ -52,37 +54,35 @@ int main(int argc, char **argv) {
         char *relative_filepath;
         assert_ok(asprintf(&relative_filepath, ".%s", http_path));
         if (is_directory(relative_filepath)) {
-            char *buffer;
-            size_t buffer_length;
-            FILE *buffer_stream = open_memstream(&buffer, &buffer_length);
-
-            DIR *d = opendir(relative_filepath);
-            render_directory_as_html(buffer_stream, d, http_path);
-            closedir(d);
-            
-            response_status = "200 OK";
-            fprintf(connection_handle, "HTTP/1.1 %s\r\nContent-Type: text/html\r\nContent-Length: %lu\r\n\r\n", response_status, buffer_length);
-            fwrite(buffer, 1, buffer_length, connection_handle);
-            fflush(connection_handle);
-
-            fclose(buffer_stream);
-            free(buffer);
+            char *html;
+            size_t html_length;
+            DIR *dir = opendir(relative_filepath);
+            render_directory_as_html(&html, &html_length, dir, http_path);
+            closedir(dir);
+            fprintf(response_stream, "HTTP/1.1 200 OK\r\nContent-Type: text/html\nContent-Length: %lu\r\n\r\n", html_length);
+            fwrite(html, 1, html_length, response_stream);
+            fflush(response_stream);
+            write(connection, response, response_length);
+            log_http_request_response(http_method, http_path, "200 OK");
+            free(html);
             goto cleanup;
         }
 
         FILE *file;
         if ((file = fopen(relative_filepath, "r")) == NULL) {
-            response_status = "404 NOT FOUND";
-            fprintf(connection_handle, "HTTP/1.1 %s\r\nContent-Length: 14\r\n\r\n404 Not Found\n", response_status);
-            fflush(connection_handle);
+            fprintf(response_stream, "HTTP/1.1 404 NOT FOUND\r\nContent-Length: 14\r\n\r\n404 Not Found\n");
+            fflush(response_stream);
+            write(connection, response, response_length);
+            log_http_request_response(http_method, http_path, "404 NOT FOUND");
             goto cleanup;
         }
 
         // Send HTTP Response Headers
         size_t filesize = read_filesize(file);
-        response_status = "200 OK";
-        fprintf(connection_handle, "HTTP/1.1 %s\r\nContent-Length: %lu\r\n\r\n", response_status, filesize);
-        fflush(connection_handle);
+        fprintf(response_stream, "HTTP/1.1 200 OK\r\nContent-Length: %lu\r\n\r\n", filesize);
+        fflush(response_stream);
+        write(connection, response, response_length);
+        log_http_request_response(http_method, http_path, "200 OK");
 
         // Send file contents to socket
         char file_buffer[FILE_BUFFER_SIZE];
@@ -93,14 +93,12 @@ int main(int argc, char **argv) {
         fclose(file);
 
     cleanup:
-        printf("[%s %s] => (%s)\n", http_method, http_path, response_status);
         free(relative_filepath);
-        free(http_status);
-        fclose(connection_handle);
         close(connection);
+        fclose(response_stream);
+        free(response);
     }
 }
-
 
 int create_ipv4_socket_and_listen(char *host, uint16_t port) {
     int reuseaddr = 1;
@@ -146,25 +144,31 @@ size_t read_filesize(FILE *file) {
 }
 
 int is_directory(const char *path) {
-    struct stat statbuf;
+    struct stat statbuf = {};
     stat(path, &statbuf);
     return S_ISDIR(statbuf.st_mode);
 }
 
-int render_directory_as_html(FILE *out, DIR *d, const char *path) {
-    fprintf(out, "<code>\n<h1>Directory: %s</h1>\n<ul>\n", path);
+int render_directory_as_html(char **html, size_t *html_length, DIR *d, const char *path) {
+    FILE *stream = open_memstream(html, html_length);
+    fprintf(stream, "<code>\n<h1>Directory: %s</h1>\n<ul>\n", path);
     struct dirent *dir;
     while ((dir = readdir(d)) != NULL) {
         size_t len = strlen(path);
-        fprintf(out, "<li><a href=\"");
+        fprintf(stream, "<li><a href=\"");
         if (len > 0 && path[len - 1] == '/') {
-            fprintf(out, "%s%s", path, dir->d_name);
+            fprintf(stream, "%s%s", path, dir->d_name);
         } else {
-            fprintf(out, "%s/%s", path, dir->d_name);
+            fprintf(stream, "%s/%s", path, dir->d_name);
         }
-        fprintf(out, "\">%s</a></li>\n", dir->d_name);
+        fprintf(stream, "\">%s</a></li>\n", dir->d_name);
     }
-    fprintf(out, "</ul>\n</code>\n");
-    fflush(out);
+    fprintf(stream, "</ul>\n</code>\n");
+    fflush(stream);
+    fclose(stream);
     return 0;
+}
+
+void log_http_request_response(char *method, char *path, char *response_status) {
+    printf("[%s %s] => (%s)\n", method, path, response_status);
 }
